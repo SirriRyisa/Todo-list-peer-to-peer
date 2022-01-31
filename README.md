e4fa61da26b53910c746fd37b5594ee48b21da95
SirriRyisa <sirriryisa@gmail.com>
2022-01-31 15:57:39 +0100

    Added this code for my peer to peer review

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Todo-list-peer-to-peer
+# ToDoList
+
+A  ToDo list with JavaScript that helps users write down list of activities they want to accomoplish within a specicific period of time.
+
+# Build with 
+ - HTML 
+ - CSS
+ - JavaScript
+
+👤 **Author**
+- GitHub: [@githubhandle](https://github.com/SirriRyisa)
+- Twitter: [@twitterhandle](https://twitter.com/N_Ryisa)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/ryisa-sirri-ngwa-a30013202)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/SirriRyisa/ToDoList/issues).
+
+# Run Project
+
+Copy from github repo and clone into your PC
+
+Run npm install to install all dependencies 
+
+npm start to run the application on a browser
+
+# Project live Demo
+
+- link [LiveDemon]( https://sirriryisa.github.io/ToDoList/)
+
+## Show your support
+
+ Give a ⭐️ if you like this project!
---
+++ src/event.js
@@ -0,0 +1,50 @@
+import {
+  addItem,
+  removeItem,
+  displayToDos,
+  getDescriptionInput,
+  markCompleted,
+  clearMethod,
+} from './index.js';
+
+const todoContainer = document.querySelector('.todos-container');
+const form = document.getElementById('form');
+const input = document.querySelector('.toDoName');
+const clearCompleted = document.querySelector('.clear-completed');
+
+window.addEventListener('load', () => {
+  displayToDos(todoContainer);
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const todoContainer = document.querySelector('.todos-container');
+  const inputVal = input.value;
+  addItem(inputVal);
+  input.value = '';
+  displayToDos(todoContainer);
+});
+
+todoContainer.addEventListener('click', (e) => {
+  if (e.target.tagName === 'I' && e.target.classList.value === 'fas fa-trash-alt') {
+    const {
+      id,
+    } = e.target;
+    removeItem(id);
+    displayToDos(todoContainer);
+  } else if (e.target.tagName === 'INPUT' && e.target.type !== 'checkbox') {
+    e.target.readOnly = false;
+
+    const {
+      id,
+    } = e.target;
+    getDescriptionInput(e.target, id);
+  } else if (e.target.tagName === 'INPUT' && e.target.type === 'checkbox') {
+    const id = e.target.id.replace('check-', '');
+    markCompleted(e.target, id, todoContainer);
+  }
+});
+
+clearCompleted.addEventListener('click', () => {
+  clearMethod(todoContainer);
+});
